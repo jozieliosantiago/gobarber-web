f55d2cb1b234194d55b899db61fd4b5eea9b016c
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-17 23:04:32 -0300

    login and registration page style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/img/logo.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
 
-export default function SignUp() {
-  return <div />;
+import logo from '~/assets/img/logo.svg';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho uma conta</Link>
+      </form>
+    </>
+  );
 }
