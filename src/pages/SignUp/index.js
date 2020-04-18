d4f3a706cf9b28968c11c0a854154533997b500a
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-17 23:24:00 -0300

    form validation

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/img/logo.svg';
 
 export default function SignIn() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required('O nome é obrigatório'),
+    email: Yup.string()
+      .email('Insira um email válido')
+      .required('O email é obrigatório'),
+    password: Yup.string()
+      .min(6, 'Mínimo 6 caracteres')
+      .required('A senha é obrigatória'),
+  });
+
   function handleSubmit(data) {
     console.log(data);
   }
@@ -13,7 +24,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="E-mail" />
         <Input name="password" type="password" placeholder="Senha" />
