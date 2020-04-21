758bcc48370ab4030e1d4dad7f0519c5bc5f95b4
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-20 23:18:09 -0300

    udpate user photo

--- src/store/modules/user/saga.js
+++ src/store/modules/user/saga.js
@@ -36,11 +36,12 @@ const notification = {
 
 export function* updateProfile({ payload }) {
   try {
-    const { name, email, ...rest } = payload.data;
+    const { name, email, avatar_id, ...rest } = payload.data;
 
     const profile = {
       name,
       email,
+      avatar_id,
       ...(rest.oldPassword ? rest : {}),
     };
 
